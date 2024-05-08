0ebb9b111e8d4d0c43e7e161f9e1fee85161022f
thanhphu0911 <thanhphu0911@gmail.com>
2024-05-08 01:50:07 -0400

    simpleApp-phusnyd

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,22 @@ react function component
 component react
 */
 
-import ReactJSX  from "./ReactJSX";
+import ReactJSX from "./ReactJSX";
 import Props from "./Props";
 import State from "./State";
 import Component from "./Component";
 import TonyComposeComponent from "./sampleApp/tony/TonyComposeComponent";
-import HandleEvent from './HandleEvent';
-
+import HandleEvent from "./HandleEvent";
+import PhuComposedComponent from "./sampleApp/phusnyd/PhuComposedComponent";
 
 function App() {
-
   // JSX
   return (
     <>
       <ReactJSX />
       <br />
       <Props />
-      
+
       <br />
       <State />
 
@@ -32,8 +31,10 @@ function App() {
 
       <br />
       <HandleEvent />
-      
+
       <br />
+      <PhuComposedComponent />
+
       <br />
       <br />
       <br />
@@ -42,7 +43,7 @@ function App() {
       <br />
       <br />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/sampleApp/phusnyd/PhuComposedComponent.jsx
@@ -0,0 +1,17 @@
+function PhuComposedComponent() {
+  return (
+    <>
+      <h1>Sample App: phusnyd - Compose Component </h1>
+      <div className="container">
+        <nav className="banner">
+          <h1>No Items!</h1>
+        </nav>
+        <main>
+          <button className="btn">Increment</button>
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default PhuComposedComponent;
